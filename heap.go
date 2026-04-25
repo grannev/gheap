87ec80fcfbcbf2df67a2fd62037a2e34e0edce01
grannev <eqorrannev@gmail.com>
2026-04-26 00:51:26 +0400

    Initialize commit
    Description: gheap (Go/Gera Heap) is a struct, that contains slice
    of memory and his amount of elements.
    
    feat: Method (Heap) AddItem(int)

---
+++ heap.go
@@ -0,0 +1,48 @@
+package heap
+
+import (
+	"fmt"
+)
+
+// ================PACKAGE SPACE=====================
+type IHeap interface {
+	AddItem(value int)
+	expandHeap(heap *Heap, count int)
+	swapItemsHeap(heap *Heap, i, j int)
+}
+
+func swapItemsHeap(heap *Heap, i, j int) {
+	heap.memory[i], heap.memory[j] =
+		heap.memory[j], heap.memory[i]
+}
+
+func expandHeap(heap *Heap, count int) {
+	heap.memory = append(heap.memory, make([]int, count)...)
+}
+
+func swapRecursive(heap *Heap, index int) {
+	var (
+		parrent_index int
+	)
+	if index == 1 {
+		return
+	}
+	parrent_index = index / 2
+	if heap.memory[index-1] > heap.memory[parrent_index-1] {
+		swapItemsHeap(heap, index-1, parrent_index-1)
+		swapRecursive(heap, parrent_index)
+	}
+	fmt.Println(heap.memory)
+}
+
+// ================USER SPACE=====================
+type Heap struct {
+	memory []int
+	size   int
+}
+
+func (heap *Heap) AddItem(value int) {
+	expandHeap(heap, 1)
+	heap.memory[heap.size-1] = value
+	swapRecursive(heap, heap.size)
+}
